12b1a43ee79117d08aa6019008ea73fb8d4ddac8
Nicolás Piován <nicolaspiovandev@gmail.com>
2022-07-12 20:56:18 -0300

    Comiendo de desafio NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+//UpperCamelCase
+import './NavBar.scss'
+
+const NavBar = () => {
+    return(
+        <div>
+            <h1>Logo</h1>
+        
+            <ul>
+            <li><button>Inicio</button></li>
+            <li><button>Productos</button></li>
+            <li><button>About us</button></li>
+            <li><button>Contacto</button></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
